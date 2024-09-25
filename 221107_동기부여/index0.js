b07b038e87e1752c4c333be9064783200b08e279
hanung kim <69525437+Kim-Hanung@users.noreply.github.com>
2024-09-26 02:23:34 +0900

    Add files via upload

---
+++ 221107_동기부여/index0.js
@@ -0,0 +1,19 @@
+<script language="JavaScript">
+function printTime() {
+var clock = document.getElementById("clock");
+var now = new Date();
+
+clock.innerHTML = now.getFullYear() + "년 " +
+(now.getMonth()+1) + "월 " +
+now.getDate() + "일 " +
+now.getHours() + "시 " +
+now.getMinutes() + "분 " +
+now.getSeconds() + "초";
+
+setTimeout("printTime()", 1000);
+}
+
+window.onload = function() {
+printTime();
+};
+</script>
